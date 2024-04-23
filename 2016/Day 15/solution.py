579540ccd0c32fe8cb9e769da55d7f739ee83108
Lunacia <130038743+LunaciaDev@users.noreply.github.com>
2024-04-23 03:42:28 +0000

    2016 Day 15

---
+++ 2016/Day 15/solution.py
@@ -0,0 +1,57 @@
+from math import lcm
+
+inputFile = open("input")
+inputStream = inputFile.read()
+
+inputStream = inputStream.strip().split("\n")
+
+discInformation = []
+
+for disc in inputStream:
+    disc = disc.split(" ")
+    discInformation.append([int(disc[3]), int(disc[-1][:-1])])
+
+def taskOne(inputStream):
+    minimumTime = 0
+    revVal = 1
+    discOffset = 1
+
+    for disc in discInformation:
+        currentDiscPosition = (disc[1] + minimumTime) % disc[0]
+        targetPosition = disc[0] - discOffset
+        if targetPosition < 0: targetPosition += disc[0]
+
+        while currentDiscPosition != targetPosition:
+            minimumTime += revVal
+            currentDiscPosition += revVal
+            currentDiscPosition %= disc[0]
+        
+        revVal = lcm(revVal, disc[0])
+        discOffset += 1
+    
+    return minimumTime
+
+def taskTwo(inputStream):
+    discInformation.append([11, 0])
+    
+    minimumTime = 0
+    revVal = 1
+    discOffset = 1
+
+    for disc in discInformation:
+        currentDiscPosition = (disc[1] + minimumTime) % disc[0]
+        targetPosition = disc[0] - discOffset
+        if targetPosition < 0: targetPosition += disc[0]
+
+        while currentDiscPosition != targetPosition:
+            minimumTime += revVal
+            currentDiscPosition += revVal
+            currentDiscPosition %= disc[0]
+        
+        revVal = lcm(revVal, disc[0])
+        discOffset += 1
+    
+    return minimumTime
+
+print(taskOne(inputStream)) # 317371
+print(taskTwo(inputStream)) # 2080951
